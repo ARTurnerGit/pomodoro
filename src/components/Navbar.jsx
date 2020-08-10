849e768901bdc6f2a5ff8f74ce1ae78f9ca7884b
Alun Turner <alun.turner@googlemail.com>
2020-08-10 13:38:41 +0100

    added link functionality to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Box, Button } from "@material-ui/core";
+import { Link } from "@reach/router";
 
 const Navbar = () => {
   return (
     <Box>
-      <Button>I'm the timer link</Button>
-      <Button>I'm the settings link</Button>
+      <Link to="/timer">
+        <Button>TIMER</Button>
+      </Link>
+      <Link to="/settings">
+        <Button>SETTINGS</Button>
+      </Link>
     </Box>
   );
 };
