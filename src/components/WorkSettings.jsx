55f4507ba29aab35e23e21ddbbbce25f4c360325
Alun Turner <alun.turner@googlemail.com>
2020-08-10 16:45:52 +0100

    added bounds to slider in worksettings

--- src/components/WorkSettings.jsx
+++ src/components/WorkSettings.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Paper, Slider, TextField } from "@material-ui/core";
 
 const WorkSettings = () => {
+  const [workDuration, setWorkDuration] = useState(25 * 60);
+  const marks = [
+    { value: 5, label: "5 mins" },
+    { value: 25, label: "25 mins" },
+    { value: 60, label: "1 hour" },
+  ];
   return (
     <Paper style={{ width: "40%", margin: "4vh 0" }}>
       Work Duration
-      <Slider />
+      <Slider
+        defaultValue="25"
+        marks={marks}
+        min={5}
+        max={60}
+        step={5}
+        valueLabelDisplay="on"
+      />
       Rounds
       <Slider />
       Work Message
