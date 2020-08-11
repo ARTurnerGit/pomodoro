114af5fe4bf32b1b3c4386285ddd03b33bde735c
Alun Turner <alun.turner@googlemail.com>
2020-08-11 09:46:31 +0100

    added values to break sliders

--- src/components/BreakSettings.jsx
+++ src/components/BreakSettings.jsx
@@ -2,12 +2,38 @@ import React from "react";
 import { Paper, Slider, TextField } from "@material-ui/core";
 
 const BreakSettings = () => {
+  const shortBreakMarks = [
+    { value: 5, label: "5 mins" },
+    { value: 10, label: "10 mins" },
+    { value: 20, label: "20 mins" },
+    { value: 30, label: "30 mins" },
+  ];
+  const longBreakMarks = [
+    { value: 10, label: "10 mins" },
+    { value: 20, label: "20 mins" },
+    { value: 40, label: "40 mins" },
+    { value: 60, label: "1 hour" },
+  ];
   return (
     <Paper style={{ width: "40%", margin: "4vh 0" }}>
       Short Break Duration
-      <Slider />
+      <Slider
+        defaultValue="5"
+        marks={shortBreakMarks}
+        min={5}
+        max={30}
+        step={1}
+        valueLabelDisplay="on"
+      />
       Long Break Duration
-      <Slider />
+      <Slider
+        defaultValue="20"
+        marks={longBreakMarks}
+        min={10}
+        max={60}
+        step={1}
+        valueLabelDisplay="on"
+      />
       Break Message
       <TextField />
     </Paper>
--- src/components/WorkSettings.jsx
+++ src/components/WorkSettings.jsx
@@ -3,24 +3,36 @@ import { Paper, Slider, TextField } from "@material-ui/core";
 
 const WorkSettings = () => {
   const [workDuration, setWorkDuration] = useState(25 * 60);
-  const marks = [
-    { value: 5, label: "5 mins" },
+  const workDurationMarks = [
+    { value: 15, label: "15 mins" },
     { value: 25, label: "25 mins" },
     { value: 60, label: "1 hour" },
+  ];
+  const roundMarks = [
+    { value: 1, label: "1" },
+    { value: 5, label: "5" },
+    { value: 15, label: "15" },
   ];
   return (
     <Paper style={{ width: "40%", margin: "4vh 0" }}>
       Work Duration
       <Slider
         defaultValue="25"
-        marks={marks}
-        min={5}
+        marks={workDurationMarks}
+        min={15}
         max={60}
         step={5}
         valueLabelDisplay="on"
       />
       Rounds
-      <Slider />
+      <Slider
+        defaultValue="5"
+        marks={roundMarks}
+        min={1}
+        max={15}
+        step={1}
+        valueLabelDisplay="on"
+      />
       Work Message
       <TextField />
     </Paper>
